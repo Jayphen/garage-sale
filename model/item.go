30ecbb65db38eb422d65c40477a234604321d567
Jayphen Simpson <jayphen.simpson@n26.com>
2024-03-24 13:23:18 +0100

    Add image thumbs

--- model/item.go
+++ model/item.go
@@ -4,15 +4,19 @@ import (
 	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase/daos"
 	"github.com/pocketbase/pocketbase/models"
+	"github.com/pocketbase/pocketbase/tools/types"
 )
 
 type Item struct {
 	models.BaseModel
-	Id          string `db:"id" json:"id"`
-	Title       string `db:"title" json:"title"`
-	Description string `db:"description" json:"description"`
-	Price       int    `db:"price" json:"price"`
+	Id          string                  `db:"id" json:"id"`
+	Title       string                  `db:"title" json:"title"`
+	Description string                  `db:"description" json:"description"`
+	Price       int                     `db:"price" json:"price"`
+	Images      types.JsonArray[string] `db:"images" json:"images"`
 }
+
+var _ models.Model = (*Item)(nil)
 
 func (m *Item) TableName() string {
 	return "items"
